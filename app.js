e250fb27a0f90865aae4bd24267f38de26d81247
yiyun <ccjhs70325@gmail.com>
2023-03-31 18:32:27 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOvervide = require('method-override')
 
 const routes = require('./routes')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
 
 const port = 3000
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 資料庫連接狀態
-const db = mongoose.connection
-db.on('error', () => console.log('mongodb error!'))
-db.once('open', () => console.log('mongodb connected!'))
+
+
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,9 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurants')
 const restaurantList = require('../../restaurant.json').results
+const db = require('../../config/mongoose')
 
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => console.log('mongodb error!'))
 db.once('open', () => {
-  console.log('mongodb connceted!')
   for (let i = 0; i < restaurantList.length; i++) {
     Restaurant.create({
       name: `${restaurantList[i].name}`,
