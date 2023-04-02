e5e83e5c92ff43053555a4b128371eb2a054c532
yiyun <ccjhs70325@gmail.com>
2023-04-02 22:11:01 +0800

    refactor routes

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,4 +9,30 @@ router.get('/', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+router.get('/search', (req, res) => {
+  const keywords = req.query.keyword
+  const keyword = keywords.trim().toLowerCase()
+
+  const sort = req.query.sort
+  const sortKeyword = {}
+
+  if (sort) {
+    const sortArray = sort.split('-')
+    const key = sortArray[0]
+    const value = sortArray[1]
+    sortKeyword[key] = value
+  }
+
+  Restaurant.find({})
+    .lean()
+    .sort(sortKeyword)
+    .then(restaurant => {
+      const filterRestaurant = restaurant.filter(data => {
+        return data.name.toLowerCase().includes(keywords) || data.category.includes(keyword)
+      })
+      
+      res.render('index', { restaurants: filterRestaurant, keyword })
+    })
+})
+
 module.exports = router
