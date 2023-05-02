81cb5bc9b128f2f709b5070efa3f5f9635c7a429
yiyun <ccjhs70325@gmail.com>
2023-05-02 17:52:48 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 const home = require('./modules/home')
 
 router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
 
 module.exports = router
